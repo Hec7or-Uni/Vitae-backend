56ae4e7131368f27ac07408b349a4c6c787e9e32
Kotopulaki <alvaro2garcia.dobon@gmail.com>
2022-04-06 00:49:33 +0200

    Swagger funciona

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const swaggerJSDoc = require('swagger-jsdoc')
 
 const routes = './app_server/routes/'
 
@@ -9,6 +10,31 @@ const indexRouter = require(routes + 'index')
 const usersRouter = require(routes + 'users')
 
 const app = express()
+// swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'API de gestión de usuarios',
+    version: '1.0.0',
+    description: 'Descripción del API del servicio de usuarios'
+  },
+  host: 'localhost:3000',
+  basePath: '/api/',
+  schemes: ['http']
+}
+// options for the swagger docs
+const options = {
+// import swaggerDefinitions
+  swaggerDefinition: swaggerDefinition,
+  // path to the API docs
+  apis: ['./app_server/routes/*.js']
+}
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options)
+// serve swagger
+app.get('/swagger.json', function (req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(swaggerSpec)
+})
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -16,7 +42,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/api/', indexRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,27 @@ const router = express.Router()
 const ctrlHome = require('../controllers/index')
 
 /* GET home page. */
+/**
+* @openapi
+* /:
+*   get:
+*     description: Welcome to this example!
+*     responses:
+*       200:
+*         description: Returns a simple message ;)
+*/
 router.get('/', ctrlHome.index)
+
+/* POST home page. */
+/**
+* @openapi
+* /:
+*   post:
+*     description: Welcome to this example!
+*     responses:
+*       200:
+*         description: Give a simple message ;)
+*/
+router.post('/', ctrlHome.index)
 
 module.exports = router
