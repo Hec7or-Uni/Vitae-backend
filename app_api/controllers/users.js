1b1660e2ad1ff662726149e9cbfcfbeddfefd734
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-05-18 16:55:04 +0200

    users changed

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const Recipe = mongoose.model('Recipe')
 
-const getStorage = function (req, res) {
+const getStorage = (req, res) => {
   const values = {
     savedRecipes: 1
   }
@@ -21,7 +21,7 @@ const getStorage = function (req, res) {
     })
 }
 
-const getMenu = function (req, res) {
+const getMenu = (req, res) => {
   const values = {
     menu: 1
   }
@@ -38,7 +38,7 @@ const getMenu = function (req, res) {
       res.status(200).json(menu)
     })
 }
-const modify = function (req, res) {
+const modify = (req, res) => {
   User.findByIdAndUpdate(req.user._id, req.body, { new: true }.exec((err, userModify) => {
     if (!userModify) {
       res.status(400).json({ message: 'User not found' })
@@ -50,7 +50,7 @@ const modify = function (req, res) {
     res.status(200).json(userModify)
   }))
 }
-const addWeight = function (req, res) {
+const addWeight = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { $push: { weight: req.body } }
@@ -66,7 +66,7 @@ const addWeight = function (req, res) {
       }))
 }
 
-const addStorage = function (req, res) {
+const addStorage = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { $push: { savedRecipes: req.body } }.exec((err, userModify) => {
@@ -80,7 +80,7 @@ const addStorage = function (req, res) {
       res.status(200).json(userModify)
     }))
 }
-const addMenuItem = function (req, res) {
+const addMenuItem = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { $push: { menu: req.body } }.exec((err, userModify) => {
@@ -95,7 +95,7 @@ const addMenuItem = function (req, res) {
     }))
 }
 
-const modifyMenu = function (req, res) {
+const modifyMenu = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { menu: req.body }.exec((err, userModify) => {
@@ -110,7 +110,7 @@ const modifyMenu = function (req, res) {
     }))
 }
 
-const deleteUser = function (req, res) {
+const deleteUser = (req, res) => {
   Recipe.findByIdAndDelete(req.user._id).exec((err, userDeleted) => {
     if (!userDeleted) {
       res.status(400).json({ message: 'User not found' })
@@ -122,7 +122,7 @@ const deleteUser = function (req, res) {
     res.status(200)
   })
 }
-const getDailyBuy = function (req, res) {}
+const getDailyBuy = (req, res) => {}
 const getUser = async (req, res) => {
   const values = {
     name: 1,
