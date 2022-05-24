736c0062f7d6adc5454ea620f323f6dff6542c47
Kotopulaki <alvaro2garcia.dobon@gmail.com>
2022-05-24 03:01:53 +0200

    works?

--- app_api/controllers/recipes.js
+++ app_api/controllers/recipes.js
@@ -67,8 +67,9 @@ const getRandomRecipe = async (req, res) => {
 }
 
 const searchRecipe = async (req, res) => {
+  const user = await User.findOne({ email: req.query.email })
   winston.info({ label: '/inventory', message: 'search-recipe:' + req.search })
-  const query = { query: req.query, dieta: req.diet }
+  const query = { query: req.query.query, dieta: user.diet }
   const data = await spoon.searchRecipes(query)
   res.status(200).json(data)
 }
